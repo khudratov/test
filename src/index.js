489ea409faebecf4e42b2331b5788e7cd47c2cb0
Mirjalol <khudratovm@mail.com>
2022-02-24 18:11:00 +0500

    clean project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Clean Project</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
