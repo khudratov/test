6bedab8b24374f9af2e0d31d88d968e03d6de2c4
Mirjalol <khudratovm@mail.com>
2022-02-24 18:33:08 +0500

    enabled jsx

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,7 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 module.exports = {
+  resolver: {
+    sourceExts: ['jsx', 'js'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
