4d0fe940bd2ae77c2d693f939b089ddb49d36e37
Mirjalol <khudratovm@mail.com>
2022-02-25 20:50:11 +0500

    setting lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@babel/eslint-parser',
+  extends: ['@react-native-community', 'plugin:react/recommended'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    semi: 'off',
+    'jsx-quotes': 'prefer-single',
+    'comma-dangle': 'off',
+    'react-hooks/exhaustive-deps': 0,
+    'react-native/no-inline-styles': 0,
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {props: 'never', children: 'never'},
+    ],
+  },
 };
