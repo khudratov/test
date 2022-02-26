fe1fda97c64299369e648b2832cf20f78e52dbf2
Mirjalol <khudratovm@mail.com>
2022-02-26 07:55:53 +0500

    created save logic

--- src/router/screen/PostsScreen.jsx
+++ src/router/screen/PostsScreen.jsx
@@ -1,23 +1,56 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useEffect } from 'react'
-import { SafeAreaView, FlatList, View } from 'react-native'
+import React, { useEffect, useState, useMemo } from 'react'
+import {
+  SafeAreaView,
+  FlatList,
+  View,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
 import { PostItem } from '../../shared/components'
 
 import { Container, Space, Text } from '../../shared/components/common'
 import { Loading } from '../../shared/components'
-import { Fonts, THEME } from '../../shared/theme'
+import { THEME, COLORS } from '../../shared/theme'
 import { useGetRequest } from '../../utils/apiService'
 import { useSelector } from 'react-redux'
+import { BookMarkSvg } from '../../shared/components/svg'
 
 export const PostsScreen = () => {
   const navigation = useNavigation()
   const { response, request, loading } = useGetRequest({ url: 'posts' })
+  const [posts, setPosts] = useState([])
   const state = useSelector((state) => state)
+  const [showSavedPost, setShowSavedPost] = useState(false)
 
-  console.log(state)
+  const renderItem = ({ item }) => (
+    <View>
+      <PostItem
+        onPress={() => handleSubmit(item.id)}
+        title={item.title}
+        description={item.description}
+        author={item.author}
+        commets={item.commets}
+      />
+      <Space height={10} />
+    </View>
+  )
 
   useEffect(() => {
-    request()
+    if (showSavedPost) {
+      const filtered = response.filter((post) => {
+        return state.posts.savedPosts.includes(post.id)
+      })
+      setPosts(filtered)
+    } else {
+      setPosts(response)
+    }
+  }, [showSavedPost])
+
+  useEffect(() => {
+    request().then((data) => {
+      setPosts(data.response)
+    })
   }, [])
 
   const handleSubmit = (id) => {
@@ -31,29 +64,28 @@ export const PostsScreen = () => {
   return (
     <SafeAreaView style={THEME.Flex}>
       <Container style={THEME.Flex}>
-        <Text size={32} font={Fonts.ManropeBold} mb={20} mt={10}>
-          Posts
-        </Text>
+        <TouchableOpacity
+          style={styles.buttonSavedPost}
+          onPress={() => setShowSavedPost(!showSavedPost)}
+        >
+          <BookMarkSvg
+            size={45}
+            color={showSavedPost ? COLORS.Secondary : COLORS.GrayTextDarker}
+          />
+        </TouchableOpacity>
 
         <FlatList
-          data={response}
-          renderItem={({ item }) => {
-            return (
-              <View key={item.id}>
-                <PostItem
-                  onPress={() => handleSubmit(item.id)}
-                  title={item.title}
-                  description={item.description}
-                  author={item.author}
-                  commets={item.commets}
-                />
-                <Space height={10} />
-              </View>
-            )
-          }}
+          data={posts}
+          renderItem={renderItem}
           keyExtractor={(item) => item.id}
         />
       </Container>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  buttonSavedPost: {
+    padding: 20
+  }
+})
